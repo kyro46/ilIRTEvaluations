2a4f39f81461ae978ea0986f7984333557fc1fb3
kyro46 <cjobst@wifa.uni-leipzig.de>
2021-06-19 15:18:42 +0200

    Initial commit for GRM-calculation via package MIRT

---
+++ classes/class.ilExteEvalOpenCPU_IRT_poly_01_GRM_MIRT.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * Calculates GRM-parameter via OpenCPU and MIRT
+ * TODO Handle questions removed due to zero variance
+ * TODO Restructure to insert a NA-row of type text instead of 0
+ * TODO Gives an evaluation of the model-fit
+ */
+class ilExteEvalOpenCPU_IRT_poly_01_GRM_MIRT extends ilExteEvalTest
+{
+	/**
+	 * @var bool	evaluation provides a single value for the overview level
+	 */
+	protected $provides_value = false;
+	
+	/**
+	 * @var bool	evaluation provides data for a details screen
+	 */
+	protected $provides_details = true;
+	
+	/**
+	 * @var bool    evaluation provides custom HTML
+	 */
+	protected $provides_HTML = true;
+	
+	/**
+	 * @var array list of allowed test types, e.g. array(self::TEST_TYPE_FIXED)
+	 */
+	protected $allowed_test_types = array();
+	
+	/**
+	 * @var array	list of question types, e.g. array('assSingleChoice', 'assMultipleChoice', ...)
+	 */
+	protected $allowed_question_types = array();
+	
+	/**
+	 * @var string	specific prefix of language variables (lowercase classname is default)
+	 */
+	protected $lang_prefix = 'tst_OpenCPUPolytomousGRM';
+	
+	/**
+	 * Calculate and classify alpha per removed item
+	 *
+	 * @return ilExteStatDetails
+	 */
+	public function calculateDetails()
+	{
+		$details = new ilExteStatDetails();
+
+		// check minimum number of participants
+		$number_of_users = count($this->data->getAllParticipants());
+		if ($number_of_users < 2)
+		{
+			$details->customHTML = $this->plugin->txt('tst_OpenCPU_calculation_error');
+			return $details;
+		}
+
+		$plugin = new ilExtendedTestStatisticsPlugin;
+		$config = $plugin->getConfig()->getEvaluationParameters("ilExteEvalOpenCPU");
+		$server = $config['server'];
+		
+		$data = ilExteEvalOpenCPU::getBasicData($this); //TRUE -> dichotomize at 50% of reachable points
+		$columnsLegend = intdiv(count($this->data->getAllQuestions()),10);
+		$path = "/ocpu/library/base/R/identity";
+		
+		$query["x"] =
+			"library(mirt);" .
+			"data <- read.csv(text='{$data['csv']}', row.names = 1, header= TRUE);" .
+			"fit <- mirt(data, 1, itemtype='graded');" . 	//constrained?
+			"coef <- coef(fit);" .
+			"plot_trace <- plot(fit, type = 'trace');" . 
+			"plot_infoSE <- plot(fit, type = 'infoSE');" .
+			"plot_info <- plot(fit, type = 'info');" .
+			"plot_SE <- plot(fit, type = 'SE');" .
+			"plot_expected_score <- plot(fit);";
+				
+		$session = ilExteEvalOpenCPU::callOpenCPU($server, $path, $query);
+		
+		if ($session == FALSE) {
+			$details->customHTML = $this->plugin->txt('tst_OpenCPU_unreachable');
+			return $details;
+		}
+		
+		//prepare results
+		$needles = array('coef','plot_trace','plot_infoSE','plot_info','plot_SE','plot_expected_score' );
+		$results = ilExteEvalOpenCPU::retrieveData($server, $session, $needles);
+		
+		$serialized = json_decode(stripslashes($results['coef']),TRUE);
+		$results['plot_infoSE'];
+		$results['plot_trace'];
+		$results['plot_expected_score'];
+		
+		// create accordions for plots and textual summaries
+		// Expected Score
+		$template = new ilTemplate('tpl.il_exte_stat_OpenCPU_Plots.html', TRUE, TRUE, "Customizing/global/plugins/Modules/Test/Evaluations/ilIRTEvaluations");
+		$template->setCurrentBlock("accordion_plot");
+		$template->setVariable('TITLE', 'Erwartete Punktzahl in Abhängigkeit von der Fähigkeitsausprägung');
+		$plot = '';
+		$plot = "<img src='data:image/png;base64," . $results['plot_expected_score'] . "'>";
+		$template->setVariable('PLOT', $plot);
+		$template->parseCurrentBlock("accordion_plot");
+
+		// Testinformation + standard error
+		$template->setCurrentBlock("accordion_plot");
+		$template->setVariable('TITLE', 'Testinformationskurve und Standardfehler');
+		$plot = '';
+		$plot = "<img src='data:image/png;base64," . $results['plot_infoSE'] . "'>";
+		$template->setVariable('PLOT', $plot);
+		$template->parseCurrentBlock("accordion_plot");
+		
+		// Tracelines
+		$template->setCurrentBlock("accordion_plot");
+		$template->setVariable('TITLE', 'Itemcharakteristikkurven');
+		$plot = '';
+		$plot = "<img src='data:image/png;base64," . $results['plot_trace'] . "'>";
+		$template->setVariable('PLOT', $plot);
+		$template->parseCurrentBlock("accordion_plot");
+		
+		
+		$template->get();
+		
+		//prepare and create output of plots
+		$customHTML = $template->get();
+
+		$details->customHTML = $customHTML;
+		//$details->customHTML .= $customHTML; //this is how you add more templates to evaluation
+		
+		//header
+		$details->columns = array (
+				ilExteStatColumn::_create('question_id', $this->plugin->txt('tst_OpenCPU_table_id'),ilExteStatColumn::SORT_NUMBER),
+				ilExteStatColumn::_create('question_title', $this->plugin->txt('tst_OpenCPU_table_title'),ilExteStatColumn::SORT_NUMBER),
+				ilExteStatColumn::_create('grm_difficulty_mean', $this->plugin->txt('tst_OpenCPUPolytomousGRM_table_Diff'),ilExteStatColumn::SORT_NUMBER),
+				ilExteStatColumn::_create('grm_disc', $this->plugin->txt('tst_OpenCPUPolytomousGRM_table_Disc'), ilExteStatColumn::SORT_NUMBER),
+		);
+		
+		//rows
+		foreach ($this->data->getAllQuestions() as $question)
+		{
+			
+			if(!$data['dichotomous']) {
+				//Questions in response can be != questions in test due to removing questions with 0 variance!
+				if(!array_key_exists('X'.$question->question_id,$serialized)){
+					$serialized['X'.$question->question_id][0] = array(0, 0);
+				}
+				
+				//calculate mean difficulty according to proposal 1 from [Usama, Chang, Anderson, 2015, DOI:10.1002/ets2.12065]
+				$disc[0] = $serialized['X' . $question->question_id][0][0];
+				$sum = array_sum($serialized['X'.$question->question_id][0]) - $disc[0];				
+				$mean = $sum / (count($serialized['X'.$question->question_id][0])-1);
+			} else {
+				if(in_array($question->question_id,$data['removed'])){
+					$disc[0] = 0;
+					$mean = 0;
+				} else {
+					$disc[0] = $serialized['X' . $question->question_id][0][0];
+					$mean = $serialized['X' . $question->question_id][0][1];
+				}
+				$i++;
+			}
+			
+			$details->rows[] = array(
+					'question_id' => ilExteStatValue::_create($question->question_id, ilExteStatValue::TYPE_NUMBER, 0),
+					'question_title' => ilExteStatValue::_create($question->question_title, ilExteStatValue::TYPE_TEXT, 0),
+					'grm_difficulty_mean' => ilExteStatValue::_create($mean, ilExteStatValue::TYPE_NUMBER, 3),
+					'grm_disc' => ilExteStatValue::_create($disc[0], ilExteStatValue::TYPE_NUMBER, 3, NULL),
+					
+			);
+		}
+		
+		return $details;
+	}
+}
